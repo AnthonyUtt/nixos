b1ba10431264dee67006b9c4d44fa202c9992af4
AnthonyUtt <anthony@anthonyutt.dev>
2024-09-04 13:22:59 -0400

    add auto gc

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -8,5 +8,12 @@
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
+
+    # Enable automatic garbage collection
+    gc = {
+      automatic = true;
+      randomizedDelaySec = "14m";
+      options = "--delete-older-than 3d";
+    };
   };
 }
