8cd7d21c1048971074f0ed458572d10a4724cb2d
AnthonyUtt <anthony@anthonyutt.dev>
2024-12-31 07:53:20 -0500

    temp: remove vpn killswitch

--- hosts/common/optional/wireguard/default.nix
+++ hosts/common/optional/wireguard/default.nix
@@ -20,37 +20,37 @@ in
           persistentKeepalive = 25;
         }];
 
-        postUp = ''
-          # Mark packets on the wg0 interface
-          wg set wg0 fwmark 51820
-
-          # Forbid anything else which doesn't go through wireguard VPN on
-          # ipV4 and ipV6
-          ${pkgs.iptables}/bin/iptables -A OUTPUT \
-            ! -d 192.168.0.0/16 \
-            ! -o wg0 \
-            -m mark ! --mark $(wg show wg0 fwmark) \
-            -m addrtype ! --dst-type LOCAL \
-            -j REJECT
-          ${pkgs.iptables}/bin/ip6tables -A OUTPUT \
-            ! -o wg0 \
-            -m mark ! --mark $(wg show wg0 fwmark) \
-            -m addrtype ! --dst-type LOCAL \
-            -j REJECT
-        '';
-
-        postDown = ''
-          ${pkgs.iptables}/bin/iptables -D OUTPUT \
-            ! -o wg0 \
-            -m mark ! --mark $(wg show wg0 fwmark) \
-            -m addrtype ! --dst-type LOCAL \
-            -j REJECT
-          ${pkgs.iptables}/bin/ip6tables -D OUTPUT \
-            ! -o wg0 -m mark \
-            ! --mark $(wg show wg0 fwmark) \
-            -m addrtype ! --dst-type LOCAL \
-            -j REJECT
-        '';
+        # postUp = ''
+        #   # Mark packets on the wg0 interface
+        #   wg set wg0 fwmark 51820
+        #
+        #   # Forbid anything else which doesn't go through wireguard VPN on
+        #   # ipV4 and ipV6
+        #   ${pkgs.iptables}/bin/iptables -A OUTPUT \
+        #     ! -d 192.168.0.0/16 \
+        #     ! -o wg0 \
+        #     -m mark ! --mark $(wg show wg0 fwmark) \
+        #     -m addrtype ! --dst-type LOCAL \
+        #     -j REJECT
+        #   ${pkgs.iptables}/bin/ip6tables -A OUTPUT \
+        #     ! -o wg0 \
+        #     -m mark ! --mark $(wg show wg0 fwmark) \
+        #     -m addrtype ! --dst-type LOCAL \
+        #     -j REJECT
+        # '';
+        #
+        # postDown = ''
+        #   ${pkgs.iptables}/bin/iptables -D OUTPUT \
+        #     ! -o wg0 \
+        #     -m mark ! --mark $(wg show wg0 fwmark) \
+        #     -m addrtype ! --dst-type LOCAL \
+        #     -j REJECT
+        #   ${pkgs.iptables}/bin/ip6tables -D OUTPUT \
+        #     ! -o wg0 -m mark \
+        #     ! --mark $(wg show wg0 fwmark) \
+        #     -m addrtype ! --dst-type LOCAL \
+        #     -j REJECT
+        # '';
       };
     };
   };
