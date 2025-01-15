73f4d57b1dc94e07ea5eae8da1b78103166c49b0
AnthonyUtt <anthony@anthonyutt.dev>
2025-01-15 10:11:53 -0500

    update + fix ff defaults

--- home/anthony/features/desktop/common/firefox/default.nix
+++ home/anthony/features/desktop/common/firefox/default.nix
@@ -52,10 +52,32 @@ in
     sessionVariables.BROWSER = "firefox";
   };
 
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = [ "firefox.desktop" ];
-    "text/xml" = [ "firefox.desktop" ];
-    "x-scheme-handler/http" = [ "firefox.desktop" ];
-    "x-scheme-handler/https" = [ "firefox.desktop" ];
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "x-scheme-handler/http" = [ "firefox-devedition.desktop" ];
+      "x-scheme-handler/https" = [ "firefox-devedition.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox-devedition.desktop" ];
+      "text/html" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-htm" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-html" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-shtml" = [ "firefox-devedition.desktop" ];
+      "application/xhtml+xml" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-xht" = [ "firefox-devedition.desktop" ];
+    };
+    defaultApplications = {
+      "text/html" = [ "firefox-devedition.desktop" ];
+      "text/xml" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox-devedition.desktop" ];
+      "x-scheme-handler/https" = [ "firefox-devedition.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-htm" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-html" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-shtml" = [ "firefox-devedition.desktop" ];
+      "application/xhtml+xml" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox-devedition.desktop" ];
+      "application/x-extension-xht" = [ "firefox-devedition.desktop" ];
+    };
   };
 }
